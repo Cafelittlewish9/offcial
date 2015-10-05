3f98ad25b788a09c4c18462d9f4723b6b305eca4
Shekx <shichering@gmail.com>
2015-10-05 19:20:51 +0800

    忘記密碼

--- src/controller/ForgetPassword.java
+++ src/controller/ForgetPassword.java
@@ -13,47 +13,58 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.service.MemberService;
 import model.vo.MemberVO;
+import util.ServicePasswordChange;
 
 
 
 @WebServlet("/forgotPwd")
 public class ForgetPassword extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public ForgetPassword() {
-        super();
-    }
+	private MemberService service;
+	@Override
+	public void init() throws ServletException {
+		this.service = new MemberService();
+	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 	    String memberAccount = request.getParameter("memberAccount");
 	    String email = request.getParameter("memberEmail");
-	    request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=UTF-8");
+	    System.out.println(memberAccount);
+	    System.out.println(email);
 		response.setCharacterEncoding("UTF-8");
-		MemberService service = new MemberService();
+		this.service = new MemberService();
 		Map<String, String> errorMessage = new HashMap<>();			       
 	    MemberVO bean = service.getOneMember(memberAccount);
-	    if(bean!=null && bean.getMemberEmail().equals(email)){
-	    	SendEmail.sendemail(bean.getMemberAccount(),new String(bean.getMemberPassword()), bean.getMemberEmail());
+	    
+	    
+	    String replace = Math.random() * 100 + "";
+	    String newPassword = ServicePasswordChange.getMD5Endocing(replace);
+	    byte[] temp = newPassword.getBytes();
+	    
+	    boolean result = false;
+	    if(bean!=null){
+	    	result = SendEmail.sendemail(memberAccount,"xxxx", email);
 	    } else {
 	    	errorMessage.put("wrong", "使用者帳號信箱錯誤");
 	    }
-	    	    
-	    if(!errorMessage.isEmpty()){
-			request.setAttribute("ErrorMsg", errorMessage);
-			RequestDispatcher rd = request.getRequestDispatcher("GetPassword.jsp");
-			rd.forward(request,response);			
-		}else{
-			RequestDispatcher rd = request.getRequestDispatcher("GetPassword1.jsp");	
-			rd.forward(request,response);
-		}
-	    
-	    
-	    
+	    if(result) {
+	    	response.getWriter().write("{\"result\":\"true\"}");
+	    } else {
+	    	response.getWriter().write("{\"result\":\"false\"}");
+	    }
+//	    if(!errorMessage.isEmpty()){
+//			request.setAttribute("ErrorMsg", errorMessage);
+//			RequestDispatcher rd = request.getRequestDispatcher("GetPassword.jsp");
+//			rd.forward(request,response);			
+//		}else{
+//			RequestDispatcher rd = request.getRequestDispatcher("GetPassword1.jsp");	
+//			rd.forward(request,response);
+//		}
 	}
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd =  request.getRequestDispatcher("/Login2.jsp");
-		rd.forward(request, response);
-		return;	
+		this.doPost(request, response);
 	}
 }
